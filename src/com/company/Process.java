261895c7431cd1feac12d728a1619d5ec657cbcd
clowman86 <vCwzpyTJLuMz4Aw>
2021-02-22 01:04:28 -0600

    Super rough version reading processes from file and executing them all for the specified amount of times.  Need to figure out where everything should live.

--- src/com/company/Process.java
+++ src/com/company/Process.java
@@ -1,16 +1,25 @@
 package com.company;
 
-public class Process {
+public class Process implements Runnable {
     public int arrivalTime;
     public String processID;
     public int serviceTime;
     public int priority;
 
-    Process(int arrivalTime,String processID, int serviceTime, int priority){
+    Process(int arrivalTime, String processID, int serviceTime, int priority) {
         this.arrivalTime = arrivalTime;
         this.processID = processID;
         this.serviceTime = serviceTime;
         this.priority = priority;
     }
 
+    public void run() {
+        System.out.println(" Running process  " + processID + " for " + serviceTime + " seconds.");
+        try {
+            Thread.sleep((serviceTime * 1000));
+        } catch (InterruptedException ex) {
+            //Catch this later?
+        }
+        System.out.println(" Process " + processID + " has finished execution.");
+    }
 }
